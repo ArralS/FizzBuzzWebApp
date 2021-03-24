b2e01a29b6f510d26e8d1957ae8dfa2c2ef6e54f
ArralS <80539257+ArralS@users.noreply.github.com>
2021-03-24 19:58:05 +0100

    Add files via upload

---
+++ Legion1/Models/Recent.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Legion1.Models
+{
+    public class Recent
+    {
+        public String Result { get; set; }
+        public int Number { get; set; }
+        public DateTime Data { get; set; }
+
+    }
+}
